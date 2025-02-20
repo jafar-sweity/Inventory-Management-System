8add60544b04e5dafc81d0dd34fc8c3e4422c281
jafar fuad <jata1518@gmail.com>
2025-02-20 23:16:45 +0200

    Implement Menu to display menu

--- Program.cs
+++ Program.cs
@@ -1,5 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("╔═════════════════════════════════════════════════════════════╗");
-Console.WriteLine("║ Invalid input. Please enter a valid decimal number.         ║");
-Console.WriteLine("╚═════════════════════════════════════════════════════════════╝");
+﻿using Inventory_Management_System.utilities;
+
+public class Program
+{
+    public static void Main(string[] args) { 
+
+        bool exit = false;
+
+
+        while (!exit)
+        {
+            Menu.DisplayMenu();
+
+            string choice = Console.ReadLine();
+
+
+            switch(choice)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    exit = true;
+                    Console.WriteLine("Thank you for using the Inventory Management System. Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+}
