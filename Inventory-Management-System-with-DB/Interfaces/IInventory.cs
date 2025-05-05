8418288a2dfbec34576a1989bc5831422119392e
jafar fuad <jata1518@gmail.com>
2025-05-05 17:14:02 +0300

    Updated Iinventory interface to use asynchronous methods and return Task

---
+++ Inventory-Management-System-with-DB/Interfaces/IInventory.cs
@@ -0,0 +1,18 @@
+﻿using Inventory_Management_System.models;
+using System.Collections.Generic;
+
+namespace Inventory_Management_System.Interfaces
+{
+    public interface IInventory
+    {
+        Task AddProduct(Product product);
+        Task<List<Product>> GetAllProducts();
+        Task<int> Count();
+        Task<Product> SearchProductByName(string name);
+        Task DeleteProductByName(string name);
+         
+        Task<bool> EditProductName(Product product, string newName);
+        Task<bool> EditProductPrice(Product product, decimal newPrice);
+        Task<bool> EditProductQuantity(Product product, int newQuantity);
+    }
+}
