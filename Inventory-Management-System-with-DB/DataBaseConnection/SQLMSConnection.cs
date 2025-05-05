3e00d4c831444f43d100a18c8afbcd31b502f493
jafar fuad <jata1518@gmail.com>
2025-05-05 17:03:36 +0300

    Add database connection string for SQL Server in DataBaseConnection class

---
+++ Inventory-Management-System-with-DB/DataBaseConnection/SQLMSConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management_System_with_DB.DataBaseConnection
+{
+    public class SQLMSConnection
+    {
+        public static string ConnectionString => "Server = LAPTOP-49OTT63M\\SQLEXPRESS ; Database = InventoryDB ; Integrated Security = SSPI; TrustServerCertificate = True;";
+    }
+}
