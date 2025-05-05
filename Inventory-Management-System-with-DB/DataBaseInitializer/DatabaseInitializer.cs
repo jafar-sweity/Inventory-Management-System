b57e5c30d6728bd0ed28769903849d443962522b
jafar fuad <jata1518@gmail.com>
2025-05-05 17:12:12 +0300

    Add DatabaseInitializer class to manage database schema and create Products table

---
+++ Inventory-Management-System-with-DB/DataBaseInitializer/DatabaseInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management_System_with_DB.DataBaseInitializer
+{
+    public class DatabaseInitializer
+    {
+        private readonly string _connectionString = DataBaseConnection.SQLMSConnection.ConnectionString;
+
+        public DatabaseInitializer()
+        {
+            CreateProductsTableAsync().Wait();
+        }
+
+        public async Task CreateProductsTableAsync()
+        {
+            var query = """
+                        IF  object_id('Products') IS  NULL
+                        BEGIN
+                           Create TABLE Products
+                           (
+                           Name NVARCHAR(50) NOT NULL ,
+                           Quantity INT NOT NULL,
+                           Price DECIMAL(18, 2) NOT NULL
+                           )
+                        END
+                        """;
+
+            using var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            using var command = new SqlCommand(query, connection);
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+}
